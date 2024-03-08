2fa21380dd26c9dcbd86acdbf68fe7e71a40579d
Oltion-Koceku <koceku17@gmail.com>
2024-03-08 15:22:00 +0100

    [add] Bundle-01

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-Title
+Debug
 ===
+Dentro ai file ZIP allegati troverete diversi files contenenti del codice javascript. Piccolo problema: sono sbagliati! All’interno di ogni esercizio c’è un errore nascosto che non permette al codice di funzionare correttamente.
--- bundle-01/main.js
+++ bundle-01/main.js
@@ -18,30 +18,33 @@ for (let i = 0; i > 5; i++) {
 
 // ESERCIZIO 2
 function addIfEven(num) {
-    if (num % 2 = 0) {
+    if (num % 2 === 0) {
         return num + 5;
     }
     return num;
 }
 
+// in questo codice si doveva usare il confornto quindi == oppure === al posto de singolo uguale che indica un "risultato"
 
 // ESERCIZIO 3
 function loopToFive() {
-    for (let i = 0, i < 5, i++) {
+    for (let i = 0; i < 5; i++) {
         console.log(i);
     }
 }
 
+// il problema in questo caso sono le virgole, bisogna usare le doppie virgolette per il ciclo for
 
 // ESERCIZIO 4 (suggerimento: ci sono 7 errori)
 function displayEvenNumbers() {
     let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
     let evenNumbers = [];
-    for (let i = 0; i < numbers.length - 1; i++;) {
-        if (numbers % 2 = 0); {
-            evenNumbers.push(i);
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] % 2 === 0) {
+            evenNumbers.push(numbers[i]);
         }
-        return evenNumbers;
     }
+    console.log(evenNumbers);
+    return evenNumbers;
 }
 displayEvenNumbers(); // dovrebbe restituire [2,4,6,8]
